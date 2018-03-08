204abf01d5a62d4d8748c34a488f23de37250fb0
张如意 <zry_nuaa@163.com>
2018-03-08 14:11:23 +0800

    New files

---
+++ demo.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"cpabe_new/bswabe"
+)
+
+func main()  {
+	pub := new(bswabe.BswabePub)
+	msk := new(bswabe.BswabeMsk)
+	bswabe.CP_Setup(pub,msk)
+
+	attrs := "foo fim baf"
+	prv := bswabe.CP_Keygen(pub,msk,attrs)
+
+	policy := "foo bar fim 2of3 baf 1of2"
+	M := "This is test message!"
+	keyCph := bswabe.CP_Enc(pub,M,policy)
+
+
+	result := bswabe.CP_Dec(pub,prv,keyCph)
+	fmt.Print("\nresult: " + string(result))
+}
