2282807fdd9120ee185eccad6f303480a853cb4b
zrynuaa <zry_nuaa@163.com>
2018-06-28 18:26:21 +0800

    Add serializeutils

--- demo.go
+++ demo.go
@@ -10,14 +10,19 @@ func main()  {
 	msk := new(bswabe.BswabeMsk)
 	bswabe.CP_Setup(pub,msk)
 
-	attrs := "foo fim baf"
-	prv := bswabe.CP_Keygen(pub,msk,attrs)
+	attrs1 := "foo fim baf"
+	attrs2 := "foo fim baf"
+	prv1 := bswabe.CP_Keygen(pub,msk,attrs1)
+	prv2 := bswabe.CP_Keygen(pub,msk,attrs2)
 
 	policy := "foo bar fim 2of3 baf 1of2"
 	M := "This is test message!"
 	keyCph := bswabe.CP_Enc(pub,M,policy)
 
 
-	result := bswabe.CP_Dec(pub,prv,keyCph)
-	fmt.Print("\nresult: " + string(result))
+	result1 := bswabe.CP_Dec(pub,prv1,keyCph)
+	fmt.Println("\nresult1: " + string(result1))
+
+	result2 := bswabe.CP_Dec(pub,prv2,keyCph)
+	fmt.Println("\nresult2: " + string(result2))
 }
